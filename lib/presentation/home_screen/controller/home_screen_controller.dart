c44e6a7e40026752b787cc3ee748ebb5bba340e2
MeAnandCB <anandcb55@gmail.com>
2024-04-19 01:42:38 +0530

    like button

--- lib/presentation/home_screen/controller/home_screen_controller.dart
+++ lib/presentation/home_screen/controller/home_screen_controller.dart
@@ -18,13 +18,14 @@ import 'dart:developer';
 import 'package:clone_app/core/app_utils.dart';
 import 'package:clone_app/presentation/home_screen/model/home_model.dart';
 import 'package:clone_app/presentation/home_screen/service/home_service.dart';
+import 'package:clone_app/presentation/login/service/login_service.dart';
 import 'package:flutter/material.dart';
 
 class HomeController extends ChangeNotifier {
   bool isLoading = false;
   HomeModel homeModel = HomeModel();
 
-  Future<void> fetchHomeData(BuildContext context) async{
+  Future<void> fetchHomeData(BuildContext context) async {
     isLoading = true;
     notifyListeners();
     HomeService.fetchData().then((value) {
@@ -35,6 +36,19 @@ class HomeController extends ChangeNotifier {
         isLoading = false;
       } else {
         AppUtils.oneTimeSnackBar("Something Went Wrong", context: context);
+      }
+      notifyListeners();
+    });
+  }
+
+  void onlike(BuildContext context, {required String id}) {
+    HomeService.postLike(id).then((value) {
+      if (value["status"] == 1) {
+        AppUtils.oneTimeSnackBar("Success",
+            context: context, bgColor: Colors.green);
+      } else {
+        AppUtils.oneTimeSnackBar("Failed",
+            context: context, bgColor: Colors.red);
       }
       notifyListeners();
     });
